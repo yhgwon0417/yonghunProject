03174b8c47c896aa775873394517dd8530ef63b3
yonghun_company <yhwon0417@gmail.com>
2021-04-02 17:14:45 +0900

    Commit
    1

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\LCSC5-ADSL5-3\Documents\GitHub\yonghunProject\venv
+set VIRTUAL_ENV=C:\Users\LCSC5-ADSL5-3\Desktop\PycharmProject\yonghunProject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
