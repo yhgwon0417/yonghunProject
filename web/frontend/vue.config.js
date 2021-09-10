8f69699e41f12b4e6adc12df21ed25b13b873318
yonghun_company <yhwon0417@gmail.com>
2021-09-10 16:28:30 +0900

    Sumamry

--- web/frontend/vue.config.js
+++ web/frontend/vue.config.js
@@ -1,34 +1,31 @@
 module.exports = {
-    publicPath: '',
-    devServer: {
-        compress: true,
-        host:'0.0.0.0',
-        disableHostCheck: true,
-        port: process.env.VUE_APP_PORT || 3000
-    }
-}
+  publicPath: "",
+  devServer: {
+    compress: true,
+    host: "0.0.0.0",
+    disableHostCheck: true,
+    port: process.env.VUE_APP_PORT || 3000,
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) => {
+        options.transformAssetUrls = {
+          img: "src",
+          image: "xlink:href",
+          "b-avatar": "src",
+          "b-img": "src",
+          "b-img-lazy": ["src", "blank-src"],
+          "b-card": "img-src",
+          "b-card-img": "src",
+          "b-card-img-lazy": ["src", "blank-src"],
+          "b-carousel-slide": "img-src",
+          "b-embed": "src",
+        };
 
-module.exports = {
-    chainWebpack: config => {
-      config.module
-        .rule('vue')
-        .use('vue-loader')
-        .loader('vue-loader')
-        .tap(options => {
-          options.transformAssetUrls = {
-            img: 'src',
-            image: 'xlink:href',
-            'b-avatar': 'src',
-            'b-img': 'src',
-            'b-img-lazy': ['src', 'blank-src'],
-            'b-card': 'img-src',
-            'b-card-img': 'src',
-            'b-card-img-lazy': ['src', 'blank-src'],
-            'b-carousel-slide': 'img-src',
-            'b-embed': 'src'
-          }
-  
-          return options
-        })
-    }
-  }
+        return options;
+      });
+  },
+};
