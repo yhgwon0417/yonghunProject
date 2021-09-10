09ef1005f66197d8474cce0196258eff611fd643
yhgwon0417 <yhgwon0417@naver.com>
2021-09-10 13:58:22 +0900

    Revert "[Bradley] Fixing"
    This reverts commit e3d8aa74b819420a4326af6cc1716939a4fcbfd8.

--- web/frontend/vue.config.js
+++ web/frontend/vue.config.js
@@ -1,5 +1,33 @@
 module.exports = {
+    publicPath: '',
     devServer: {
+        compress: true,
+        host:'0.0.0.0',
         disableHostCheck: true
     }
 }
+
+module.exports = {
+    chainWebpack: config => {
+      config.module
+        .rule('vue')
+        .use('vue-loader')
+        .loader('vue-loader')
+        .tap(options => {
+          options.transformAssetUrls = {
+            img: 'src',
+            image: 'xlink:href',
+            'b-avatar': 'src',
+            'b-img': 'src',
+            'b-img-lazy': ['src', 'blank-src'],
+            'b-card': 'img-src',
+            'b-card-img': 'src',
+            'b-card-img-lazy': ['src', 'blank-src'],
+            'b-carousel-slide': 'img-src',
+            'b-embed': 'src'
+          }
+  
+          return options
+        })
+    }
+  }
