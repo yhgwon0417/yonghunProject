3ba1b5a76d51d6168986750df7c311f4c418ed73
yonghun_company <yhwon0417@gmail.com>
2021-09-14 14:31:52 +0900

    Update views.py

--- web/yonghun/views.py
+++ web/yonghun/views.py
@@ -27,8 +27,8 @@ class UserCreate(generics.CreateAPIView):
 
 KAKAO_REST_API_KEY = "fcdb4932bf507a42c8be3ec4d0633ded"
 SECRET_KEY = "399851"
-# BASE_URL = 'http://127.0.0.1:8000/'
-BASE_URL = HOST +"/"
+BASE_URL = 'http://yeub.iptime.org:8000/'
+# BASE_URL = HOST +"/"
 KAKAO_CALLBACK_URI = BASE_URL + 'yonghun/account/login/kakao/callback'
 GOOGLE_CALLBACK_URI = BASE_URL + 'yonghun/account/login/google/callback/'
 
